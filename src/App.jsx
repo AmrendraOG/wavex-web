ca06e715cf4e4e0f79579e062276bd72da141e36
AmrendraOG <amren.dev@gmail.com>
2025-10-04 21:01:41 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import { useState, useRef, useEffect } from 'react'
+import { FaPlay, FaPause, FaStop, FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa"
+import songs from "./data/songs.json"
+
+const App = () => {
+  const [playText, setPlayText] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [currentSong, setCurrentSong] = useState(songs[0].title)
+  const audioRef = useRef(new Audio(`https://github.com/AmrendraOG/wavex/raw/refs/heads/master/assets/songs/${currentSong}.mp3`));
+  const [artwork, setArtwork] = useState(`https://raw.githubusercontent.com/AmrendraOG/wavex/refs/heads/master/assets/covers/${currentSong}.png`);
+  const handleClick = () => {
+    if (playText) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setPlayText(!playText);
+  }
+
+  const handleStop = () => {
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
+    setPlayText(false)
+  }
+
+  useEffect(() => {
+    audioRef.current.pause();
+
+    const audio = new Audio(`https://github.com/AmrendraOG/wavex/raw/refs/heads/master/assets/songs/${currentSong}.mp3`);
+    audioRef.current = audio;
+
+    setArtwork(`https://raw.githubusercontent.com/AmrendraOG/wavex/refs/heads/master/assets/covers/${currentSong}.png`);
+
+
+    const updateProgress = () => {
+      setProgress(audio.currentTime);
+    };
+
+    audio.addEventListener("timeupdate", updateProgress);
+
+    // Reset when audio ends
+    audio.addEventListener("ended", () => setPlayText(false));
+
+    if (playText) {
+      audio.play();
+    }
+
+    return () => {
+      audio.pause();
+      audio.removeEventListener("timeupdate", updateProgress);
+      audio.removeEventListener("ended", () => setPlayText(false));
+    };
+  }, [currentSong]);
+
+  const handleSeek = (e) => {
+    const newTime = e.target.value;
+    audioRef.current.currentTime = newTime;
+    setProgress(newTime);
+  }
+
+  const handleLeft = () => {
+    audioRef.current.currentTime -= 10;
+  }
+
+  const handleRight = () => {
+    audioRef.current.currentTime += 10;
+  }
+
+  return (
+    <div className='h-screen flex flex-col'>
+      <div className='flex flex-row justify-center py-6 bg-cyan-950'>
+        <p className='text-4xl text-cyan-400 font-extrabold'>WaveX</p>
+      </div>
+      <div className='main h-screen flex flex-row'>
+        <div className='songsList w-96 bg-cyan-950 overflow-y-hidden p-8'>
+          <h1 className='text-white text-2xl bg-cyan-950 font-bold text-center p-4'>Songs List</h1>
+          <ul className='flex flex-col gap-4'>
+            {songs.map((song, index) => {
+              return (
+                <button key={index} className='p-2 rounded bg-cyan-700 text-white hover:bg-cyan-800' onClick={() => {
+                  audioRef.current = new Audio(`https://github.com/AmrendraOG/wavex/raw/refs/heads/master/assets/songs/${currentSong}.mp3`);
+                  setCurrentSong(song.title);
+                  setPlayText(true);
+                }
+                }>
+                  <strong>{song.title}</strong>
+                </button>
+              )
+            })}
+          </ul>
+        </div>
+
+        <div className='player flex-1 overflow-hidden bg-gray-900 flex flex-col justify-center items-center gap-8'>
+          <div className='artwork rounded w-80 h-80 bg-cyan-950 flex justify-center items-center'>
+            <img src={artwork} />
+          </div>
+          <div className='text-white text-3xl font-bold'>
+            {currentSong}
+          </div>
+          <div className='slider'>
+            <input
+              type='range'
+              min={0}
+              max={audioRef.current.duration || 0}
+              className='w-80 accent-cyan-500'
+              value={progress}
+              onChange={handleSeek}
+            />
+          </div>
+          <div className='buttons flex flex-row gap-8'>
+            <button className='rounded-full p-4 text-cyan-400 text-4xl font-bold hover:bg-cyan-500 hover:text-black'
+              onClick={handleLeft}
+            >
+              <FaAngleDoubleLeft />
+            </button>
+            <button className='rounded-full p-6 bg-cyan-400 text-black font-bold hover:bg-cyan-500' onClick={handleClick}>
+              {playText ? <FaPause /> : <FaPlay />}
+            </button>
+            <button className='rounded-full p-6 bg-cyan-400 text-black font-bold hover:bg-cyan-500'
+              onClick={handleStop}
+            >
+              <FaStop />
+            </button>
+            <button className='rounded-full p-4 text-cyan-400 text-4xl font-bold hover:bg-cyan-500 hover:text-black'
+              onClick={handleRight}
+            >
+              <FaAngleDoubleRight />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App

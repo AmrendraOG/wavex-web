14b7afed1b89adf525ff4cf1ad3a5c6571e19d37
AmrendraOG <amren.dev@gmail.com>
2025-10-05 18:11:10 +0530

    Added more styling

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,6 @@ const App = () => {
 
     audio.addEventListener("timeupdate", updateProgress);
 
-    // Reset when audio ends
     audio.addEventListener("ended", () => setPlayText(false));
 
     if (playText) {
@@ -81,17 +80,17 @@ const App = () => {
 
   return (
     <div className='h-screen flex flex-col'>
-      <div className='flex flex-col justify-center text-center py-6 bg-cyan-950'>
-        <p className='text-4xl text-cyan-400 font-extrabold'>WaveX</p>
+      <div className='flex flex-col justify-center text-center py-4 bg-sky-950 shadow-[0_4px_8px_rgba(0,0,0,0.5)] z-10'>
+        <p className='text-3xl text-cyan-400 font-extrabold'>WaveX</p>
         <p className='text-sm text-cyan-600 font-extrabold'>By Amrendra Maurya</p>
       </div>
       <div className='main h-screen flex flex-row'>
-        <div className='songsList w-96 bg-cyan-950 overflow-y-hidden p-8'>
+        <div className='songsList w-96 bg-cyan-950 overflow-y-hidden p-8 z-5 shadow-[4px_0_8px_rgba(0,0,0,0.5)]'>
           <h1 className='text-white text-2xl bg-cyan-950 font-bold text-center p-4'>Songs List</h1>
           <ul className='flex flex-col gap-4'>
             {songs.map((song, index) => {
               return (
-                <button key={index} className={`bg-cyan-700 p-2 rounded  text-white hover:bg-cyan-800`} onClick={() => {
+                <button key={index} className={`bg-cyan-700 cursor-pointer p-2 active:bg-cyan-900 rounded text-white hover:bg-cyan-800 shadow-[4px_2px_8px_rgba(0,0,0,0.4)]`} onClick={() => {
                   audioRef.current = new Audio(`https://github.com/AmrendraOG/wavex/raw/refs/heads/master/assets/songs/${currentSong}.mp3`);
                   setCurrentSong(song.title);
                   setPlayText(true);
@@ -107,7 +106,7 @@ const App = () => {
 
         <div className='player flex-1 overflow-hidden bg-gray-900 flex flex-col justify-center items-center gap-8'>
           <div className='artwork rounded w-80 h-80 md:w-64 md:h-64 bg-cyan-950 flex justify-center items-center'>
-            <img src={artwork} />
+            <img src={artwork} draggable="false" />
           </div>
           <div className='text-white text-3xl font-bold'>
             {currentSong}
@@ -118,25 +117,26 @@ const App = () => {
               min={0}
               max={audioRef.current.duration || 0}
               className='w-80 accent-cyan-500'
+              style={{ cursor: 'pointer' }}
               value={progress}
               onChange={handleSeek}
             />
           </div>
           <div className='buttons flex flex-row gap-8'>
-            <button className='rounded-full p-4 text-cyan-400 text-4xl font-bold hover:bg-cyan-500 hover:text-black'
+            <button className='rounded-full active:bg-cyan-600 p-4 cursor-pointer text-cyan-400 text-4xl font-bold hover:bg-cyan-500 hover:text-black'
               onClick={handleLeft}
             >
               <FaAngleDoubleLeft />
             </button>
-            <button className='rounded-full p-6 bg-cyan-400 text-black font-bold hover:bg-cyan-500' onClick={handleClick}>
+            <button className='rounded-full active:bg-cyan-600 p-6 cursor-pointer bg-cyan-400 text-black font-bold hover:bg-cyan-500' onClick={handleClick}>
               {playText ? <FaPause /> : <FaPlay />}
             </button>
-            <button className='rounded-full p-6 bg-cyan-400 text-black font-bold hover:bg-cyan-500'
+            <button className='rounded-full active:bg-cyan-600 p-6 cursor-pointer bg-cyan-400 text-black font-bold hover:bg-cyan-500'
               onClick={handleStop}
             >
               <FaStop />
             </button>
-            <button className='rounded-full p-4 text-cyan-400 text-4xl font-bold hover:bg-cyan-500 hover:text-black'
+            <button className='rounded-full active:bg-cyan-600 p-4 cursor-pointer text-cyan-400 text-4xl font-bold hover:bg-cyan-500 hover:text-black'
               onClick={handleRight}
             >
               <FaAngleDoubleRight />
@@ -149,6 +149,7 @@ const App = () => {
               max='100'
               value={volume}
               onChange={handleVolume}
+              style={{ cursor: 'pointer' }}
             />
           </div>
         </div>
